b666a7bcee19b26a1b6aa6874b2f3473b8298285
P Jaworski <pjaworski.dev@gmail.com>
2024-02-24 22:50:49 +0100

    build: updated docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,8 +25,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'matro-ui', // Usually your GitHub org/user name.
+  projectName: 'matro-ui.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
