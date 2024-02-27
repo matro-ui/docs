f9a53999180b3d35b201e2429f1a8d6fb1ad0ddd
P Jaworski <pjaworski.dev@gmail.com>
2024-02-27 21:50:18 +0100

    build: added Sources

---
+++ assets/components/Sources/Sources.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import s from './index.module.css';
+import Button from 'matro-ui/dist/other/Button';
+import { FaGithub, FaNpm } from "react-icons/fa";
+
+const Sources = ({ git, npmjs, name }) => {
+  return (
+    <div className={s.sources}>
+      <Button
+        leftIcon={<FaGithub />}
+        as={<a href={git} target='_blank' />}
+      >Source</Button>
+      <Button
+        leftIcon={<FaNpm color="#e24141" style={{ fontSize: "2em" }} />}
+        as={<a href={npmjs} target='_blank' />}>@matro-ui/{name}</Button>
+    </div >
+  )
+}
+
+export default Sources
