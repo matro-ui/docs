fee92e85aa74cb8aaabae2889fb033e8d5647682
P Jaworski <pjaworski.dev@gmail.com>
2024-03-03 18:20:59 +0100

    update: updated import, added "as" prop

--- assets/components/Sources/Sources.jsx
+++ assets/components/Sources/Sources.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import s from './index.module.css';
-import Button from 'matro-ui/dist/other/Button';
+import { Button } from 'matro-ui';
 import { FaGithub, FaNpm } from "react-icons/fa";
 
 const Sources = ({ git, npmjs, name }) => {
@@ -8,7 +8,6 @@ const Sources = ({ git, npmjs, name }) => {
     <div className={s.sources}>
       <Button
         leftIcon={<FaGithub />}
-        as={<a href={git} target='_blank' />}
       >Source</Button>
       <Button
         leftIcon={<FaNpm color="#e24141" style={{ fontSize: "2em" }} />}
@@ -17,4 +16,4 @@ const Sources = ({ git, npmjs, name }) => {
   )
 }
 
-export default Sources
+export default Sources;
