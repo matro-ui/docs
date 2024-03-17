52443eabb5a67468829cfbd4fd1e36d8139bed55
P Jaworski <pjaworski.dev@gmail.com>
2024-03-17 13:58:17 +0100

    Deploy website - based on 71bf28e76075f513433f6bbb96df06eebac6690a

---
+++ assets/js/37.62a78855.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmatro_docs=self.webpackChunkmatro_docs||[]).push([[37],{37:(t,r,o)=>{o.d(r,{xCi:()=>n});var e=o(1414);function n(t){return(0,e.k5)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"path",attr:{d:"m9 18 6-6-6-6"},child:[]}]})(t)}}}]);
